f09467435d03289477b4c54b1762de348e9a4a0d
Diomidis Spinellis <dds@aueb.gr>
2019-01-30 20:54:18 +0200

    Update for pgsql 9.6

--- modules/postgresql/manifests/init.pp
+++ modules/postgresql/manifests/init.pp
@@ -1,12 +1,25 @@
 class postgresql {
-  package{'postgresql': }
+  # Note:
+  # Control database with sudo systemctl start|stop postgresql@9.6-main.service
+
+  # Setup data under /home
+  file_line { 'postgresql data directory':
+    path    => '/etc/postgresql-common/createcluster.conf',
+    line    => "data_directory = '/home/postgresql/%v/%c'",
+    match   => "#data_directory =.*",
+    require => Package['postgresql-9.6'],
+    notify  => Exec['pg_createcluster 9.6 main']
+  }
+
+  exec {'pg_createcluster 9.6 main':
+    refreshonly => true
+  }
+
   package{'postgresql-9.6': }
   package{'postgresql-client-9.6': }
   package{'postgresql-client-common': }
   package{'postgresql-common': }
-  package{'postgresql-contrib': }
   package{'postgresql-contrib-9.6': }
-  package{'postgresql-doc': }
   package{'postgresql-doc-9.6': }
   package{'postgresql-9.6-debversion': }
 }
