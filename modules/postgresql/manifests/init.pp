0479035a6a01cb795de6a8736f5882578ec74389
Diomidis Spinellis <dds@aueb.gr>
2019-01-30 19:07:31 +0200

    Add postgresql-9.6-debversion package
    And upgrade postgresql version

--- modules/postgresql/manifests/init.pp
+++ modules/postgresql/manifests/init.pp
@@ -1,11 +1,12 @@
 class postgresql {
   package{'postgresql': }
-  package{'postgresql-9.4': }
-  package{'postgresql-client-9.4': }
+  package{'postgresql-9.6': }
+  package{'postgresql-client-9.6': }
   package{'postgresql-client-common': }
   package{'postgresql-common': }
   package{'postgresql-contrib': }
-  package{'postgresql-contrib-9.4': }
+  package{'postgresql-contrib-9.6': }
   package{'postgresql-doc': }
-  package{'postgresql-doc-9.4': }
+  package{'postgresql-doc-9.6': }
+  package{'postgresql-9.6-debversion': }
 }
